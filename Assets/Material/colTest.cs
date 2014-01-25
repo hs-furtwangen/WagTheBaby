90ac31e9cf40b4b2105b78fa630500459d69d2ee
Linda Schey <lindaschey@t-online.de>
2014-01-25 01:07:27 +0100

    Set up Project Folders, first tests to change the prefabs

---
+++ Assets/Material/colTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.
+		renderer.material.color = Color.cyan;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider collisionInfo)
+	{
+		if(collisionInfo.gameObject.tag == "Player")
+		{
+			renderer.material.color = Color.blue;
+		}
+	}
+
+}
---
+++ Assets/Scripts/DistanceChecking.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class DistanceChecking : MonoBehaviour 
+{
+			
+	public GameObject RealPrefab;
+	public GameObject EvilPrefab;
+	private MeshCollider col;
+	// Use this for initialization
+	void Start () 
+	{
+		Destroy(GetComponent<Collider>());
+		col = gameObject.AddComponent("MeshCollider") as MeshCollider;
+	
+		this.GetComponent<MeshFilter>().mesh = RealPrefab.GetComponent<MeshFilter>().mesh;
+		//this.GetComponent<Collider>().collider = RealPrefab.GetComponent<Collider>().collider;
+
+	}
+			
+	// Update is called once per frame
+	void Update () {
+	
+	}
+			
+	void OnTriggerEnter(Collider other)
+	{
+		this.GetComponent<MeshFilter>().mesh = EvilPrefab.GetComponent<MeshFilter>().mesh;
+		//this.GetComponent<Collider>().collider = EvilPrefab.GetComponent<Collider>().collider;
+	}
+			
+	void OnTriggerExit(Collider other)
+	{
+		this.GetComponent<MeshFilter>().mesh = RealPrefab.GetComponent<MeshFilter>().mesh;
+		//this.GetComponent<Collider>().collider = RealPrefab.GetComponent<Collider>().collider;
+	}
+}
