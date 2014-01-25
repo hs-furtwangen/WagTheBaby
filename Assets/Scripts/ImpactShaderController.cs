20c98394e1944e4a40c727d3cc3f991b7f4f1e48
Alexander Scheurer <alexander.scheurer@hs-furtwangen.de>
2014-01-25 20:22:48 +0100

    Shaderscript searches for Player tag

--- Assets/Scripts/ImpactShaderController.cs
+++ Assets/Scripts/ImpactShaderController.cs
@@ -7,14 +7,14 @@ public class ImpactShaderController : MonoBehaviour
     public Transform playerPosition;
 
 	// Use this for initialization
-	void Start ()
-	{
-	    //playerPosition = GameObject.Find("/Plane");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        renderer.material.SetVector("_centerPos", new Vector3(playerPosition.position.x, 0, playerPosition.position.z));
-        Debug.Log(playerPosition.position);
+    private void Start()
+    {
+        playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+	void Update ()
+    {
+        renderer.material.SetVector("_centerPos", playerPosition.position);
 	}
 }
