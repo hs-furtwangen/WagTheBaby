f885a9c3d7f2f7d089b87d852a2c4940e27c54e4
Thomas Müller <t.mueller00@googlemail.com>
2014-01-25 21:30:39 +0100

    Sounds

---
+++ Assets/Scripts/LavaParticle.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LavaParticle : MonoBehaviour {
+
+	public GameObject fpController = null;
+	private AudioSource hiss = null;
+	public GameObject lavafloor = null;
+	private float timer = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		if(fpController == null) return;
+		if(lavafloor == null) {
+			Debug.Log("Assign Lavafloor Gameobject");
+			return;
+		}
+		hiss = this.transform.gameObject.GetComponentInChildren<AudioSource>();
+		if(hiss == null) Debug.Log ("No Audisource for his available as child");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(fpController == null || lavafloor == null) return;
+		this.transform.position = new Vector3(fpController.transform.position.x, this.transform.position.y, fpController.transform.position.z);
+
+		if(hiss && lavafloor) {
+			timer -= Time.deltaTime;
+			if(timer <= 0.0f) {
+				hiss.transform.localPosition = Random.insideUnitCircle * lavafloor.renderer.material.GetFloat("_Radius1");
+				timer += Random.Range(5.0f, 10.0f);
+				hiss.Play();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/SoundTriggerSystem.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundTriggerSystem : MonoBehaviour {
+
+	// AND THIS SCRIPT TO YOUR CHARACTERCONTROLLER OR SIMLILAR TO ITS POSITION
+	public GameObject triggerObjects = null;
+	public float radius = 3.0f;
+
+
+	void Start () 
+	{
+		if(triggerObjects == null) {Application.Quit(); return;}
+	}
+
+	void Update () 
+	{
+		foreach(Transform t in triggerObjects.transform) {
+			//Debug.Log(t.gameObject.name);
+			if(Vector3.Distance(t.position, this.transform.position) <= radius) {
+				t.gameObject.GetComponent<AudioSource>().Play();
+			}
+		}
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(this.transform.position, radius);
+	}
+}
