22faacf4d2c68b05de17267570dfe14e18c77cc2
Linda Schey <lindaschey@t-online.de>
2014-01-25 02:52:21 +0100

    classes for exti and enter

---
+++ Assets/Scripts/DestroyEvil.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyEvil : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerExit()
+	{
+		Debug.Log("Exit");
+		SendMessageUpwards("ChangeInstance", this.gameObject);
+	}
+}
---
+++ Assets/Scripts/DestroyReal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyReal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	void OnTriggerEnter()
+	{
+		Debug.Log("Enter");
+		SendMessageUpwards("ChangeInstance", this.gameObject);
+	}
+
+}
