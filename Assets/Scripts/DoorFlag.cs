33507e168f951232ff952d21dce3d88f1433850f
Thomas Müller <t.mueller00@googlemail.com>
2014-01-26 09:45:09 +0100

    Audio und doortrigger

---
+++ Assets/Scripts/DoorFlag.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorFlag : MonoBehaviour {
+	private bool _isOpen = false;
+	public bool isOpen {
+		get { return _isOpen; }
+		set { _isOpen = value; }
+	}
+}
