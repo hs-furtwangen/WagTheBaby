42ae66581e333983631dd15f2224059b4542a830
Alexander Scheurer <alexander.scheurer@hs-furtwangen.de>
2014-01-26 15:21:53 +0100

    LavaCollision

---
+++ Assets/Scripts/LavaCollision.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class LavaCollision : MonoBehaviour
+{
+
+    private Vector3 RespawnPosition = new Vector3(0.5f, 1f, 0f);
+    private float raylength = 0.35f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        RaycastHit hit;
+        
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, raylength ))
+        {
+            if (hit.transform.tag == "Respawn" && GameStateHandler.CurrentGameState == (int) GameState.RunningLava)
+            {
+               transform.position = RespawnPosition;
+            }
+        }
+	}
+}
